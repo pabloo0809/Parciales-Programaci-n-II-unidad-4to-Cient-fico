ab2da7ab01356a95a5958bb1d41a6376721825ef
mr.pensaii <alguienanonimo008@gmail.com>
2026-05-05 08:54:48 -0600

    Add files via upload

---
+++ arreglos.py
@@ -0,0 +1,15 @@
+nombres = ["Juan", "Marlon", "Samanta", "Samuel"]
+edades = ["12", "14", "15", "17"]
+
+for nombre in nombres:
+    print(nombre)
+
+for edad in edades:
+    print(edad)
+
+nombres = ["Juan", "Marlon", "Samanta", "Samuel"]
+edades = ["12", "14", "15", "17"]
+
+print(nombres[4])
+
+print(edades[4])
